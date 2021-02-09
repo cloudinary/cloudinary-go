cba11355eed972b823e0ba46e5d3b249043cb911
RTLcoil <oleg@rtl.co.il>
2021-02-09 23:56:14 +0400

    Create* to New*

--- cloudinary.go
+++ cloudinary.go
@@ -19,29 +19,29 @@ func New() (*Cloudinary, error) {
 	if err != nil {
 		return nil, err
 	}
-	return CreateFromConfiguration(*c)
+	return NewFromConfiguration(*c)
 }
 
-// CreateFromUrl returns a new Cloudinary instance from a cloudinary url.
-func CreateFromUrl(cloudinaryUrl string) (*Cloudinary, error) {
-	c, err := config.CreateFromUrl(cloudinaryUrl)
+// NewFromUrl returns a new Cloudinary instance from a cloudinary url.
+func NewFromUrl(cloudinaryUrl string) (*Cloudinary, error) {
+	c, err := config.NewFromUrl(cloudinaryUrl)
 	if err != nil {
 		return nil, err
 	}
-	return CreateFromConfiguration(*c)
+	return NewFromConfiguration(*c)
 }
 
-// CreateFromParams returns a new Cloudinary instance from the provided parameters.
-func CreateFromParams(cloud string, key string, secret string) (*Cloudinary, error) {
-	c, err := config.CreateFromParams(cloud, key, secret)
+// NewFromParams returns a new Cloudinary instance from the provided parameters.
+func NewFromParams(cloud string, key string, secret string) (*Cloudinary, error) {
+	c, err := config.NewFromParams(cloud, key, secret)
 	if err != nil {
 		return nil, err
 	}
-	return CreateFromConfiguration(*c)
+	return NewFromConfiguration(*c)
 }
 
-// CreateFromConfiguration returns a new Cloudinary instance from the provided configuration.
-func CreateFromConfiguration(configuration config.Configuration) (*Cloudinary, error) {
+// NewFromConfiguration returns a new Cloudinary instance from the provided configuration.
+func NewFromConfiguration(configuration config.Configuration) (*Cloudinary, error) {
 	return &Cloudinary{
 		Config: configuration,
 		Admin: admin.Api{
--- config/configuration.go
+++ config/configuration.go
@@ -16,11 +16,11 @@ type Configuration struct {
 
 // New returns a new Configuration instance from the environment variable
 func New() (*Configuration, error) {
-	return CreateFromUrl(os.Getenv("CLOUDINARY_URL"))
+	return NewFromUrl(os.Getenv("CLOUDINARY_URL"))
 }
 
-// CreateFromUrl returns a new Configuration instance from a cloudinary url.
-func CreateFromUrl(cldUrlStr string) (*Configuration, error) {
+// NewFromUrl returns a new Configuration instance from a cloudinary url.
+func NewFromUrl(cldUrlStr string) (*Configuration, error) {
 	cldUrl, err := url.Parse(cldUrlStr)
 	if err != nil {
 		return nil, err
@@ -28,11 +28,11 @@ func CreateFromUrl(cldUrlStr string) (*Configuration, error) {
 
 	pass, _ := cldUrl.User.Password()
 
-	return CreateFromParams(cldUrl.Host, cldUrl.User.Username(), pass)
+	return NewFromParams(cldUrl.Host, cldUrl.User.Username(), pass)
 }
 
-// CreateFromParams returns a new Configuration instance from the provided parameters.
-func CreateFromParams(cloud string, key string, secret string) (*Configuration, error) {
+// NewFromParams returns a new Configuration instance from the provided parameters.
+func NewFromParams(cloud string, key string, secret string) (*Configuration, error) {
 	conf := &Configuration{
 		Account: Account{
 			CloudName: cloud,
