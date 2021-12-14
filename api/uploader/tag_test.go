c86641fdb68f5c77b10035367ad42911d19b164d
Constantine Nathanson <constantine@cloudinary.com>
2021-12-13 23:13:30 -0500

    Fix multiple `PublicIDs` support in `Tags` Upload API

--- api/uploader/tag_test.go
+++ api/uploader/tag_test.go
@@ -10,9 +10,10 @@ import (
 
 func TestUploader_Tag(t *testing.T) {
 	cldtest.UploadTestAsset(t, cldtest.PublicID)
+	cldtest.UploadTestAsset(t, cldtest.PublicID2)
 
 	params := uploader.AddTagParams{
-		PublicIDs: []string{cldtest.PublicID},
+		PublicIDs: []string{cldtest.PublicID, cldtest.PublicID2},
 		Tag:       cldtest.Tag1,
 	}
 
@@ -22,12 +23,12 @@ func TestUploader_Tag(t *testing.T) {
 		t.Error(err)
 	}
 
-	if resp == nil || len(resp.PublicIds) != 1 || resp.PublicIds[0] != cldtest.PublicID {
+	if resp == nil || len(resp.PublicIds) != 2 || resp.PublicIds[0] != cldtest.PublicID {
 		t.Error(resp)
 	}
 
 	rParams := uploader.RemoveTagParams{
-		PublicIDs: []string{cldtest.PublicID},
+		PublicIDs: []string{cldtest.PublicID, cldtest.PublicID2},
 		Tag:       cldtest.Tag1,
 	}
 
@@ -37,12 +38,12 @@ func TestUploader_Tag(t *testing.T) {
 		t.Error(err)
 	}
 
-	if rResp == nil || len(rResp.PublicIds) != 1 || rResp.PublicIds[0] != cldtest.PublicID {
+	if rResp == nil || len(rResp.PublicIds) != 2 || rResp.PublicIds[0] != cldtest.PublicID {
 		t.Error(resp)
 	}
 	// FIXME: add some tags :) before removing
 	raParams := uploader.RemoveAllTagsParams{
-		PublicIDs: api.CldAPIArray{cldtest.PublicID},
+		PublicIDs: api.CldAPIArray{cldtest.PublicID, cldtest.PublicID2},
 	}
 
 	raResp, err := uploadAPI.RemoveAllTags(ctx, raParams)
@@ -51,12 +52,12 @@ func TestUploader_Tag(t *testing.T) {
 		t.Error(err)
 	}
 
-	if raResp == nil || len(raResp.PublicIds) != 1 || raResp.PublicIds[0] != cldtest.PublicID {
+	if raResp == nil || len(raResp.PublicIds) != 2 || raResp.PublicIds[0] != cldtest.PublicID {
 		t.Error(resp)
 	}
 
 	reParams := uploader.ReplaceTagParams{
-		PublicIDs: api.CldAPIArray{cldtest.PublicID},
+		PublicIDs: api.CldAPIArray{cldtest.PublicID, cldtest.PublicID2},
 		Tag:       cldtest.Tag2,
 	}
 
@@ -66,7 +67,7 @@ func TestUploader_Tag(t *testing.T) {
 		t.Error(err)
 	}
 
-	if reResp == nil || len(reResp.PublicIds) != 1 || reResp.PublicIds[0] != cldtest.PublicID {
+	if reResp == nil || len(reResp.PublicIds) != 2 || reResp.PublicIds[0] != cldtest.PublicID {
 		t.Error(resp)
 	}
 }
